7711113ea6ac20a0e83396c363aa07e11788b1e1
Jianyuan <vincent.zhang@logicmonitor.com>
2018-09-09 21:43:19 +0800

    # register page

--- pages/register/register.js
+++ pages/register/register.js
@@ -3,5 +3,62 @@ const app = getApp()
 Page({
   data: {
     
+  },
+  doRegist: function(e) {
+    var formObj = e.detail.value;
+    var username = formObj.username;
+    var password = formObj.password;
+
+    // simple validate
+    if (username.length == 0 || password.length == 0) {
+      wx.showToast({
+        title: '用户名和密码不能为空',
+        icon: 'none',
+        duration: 3000
+      })
+    }
+    else {
+      var serverUrl = app.serverUrl + "/register/";
+      console.log("POST " + serverUrl);
+      wx.request({
+        url: serverUrl,
+        method: "POST",
+        data: {
+          username: username,
+          password: password
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function(res) {
+          var httpStatus = res.statusCode;
+          if (httpStatus != 200) {
+            wx.showToast({
+              title: 'HTTP错误：' + httpStatus,
+              icon: "none",
+              duration: 3000
+            })
+          }
+          else {
+            var status = res.data.status;
+            if (status != 200) {
+              wx.showToast({
+                title: res.data.errMsg,
+                icon: "none",
+                duration: 3000
+              })
+            }
+            else {
+              wx.showToast({
+                title: '注册成功',
+                duration: 3000
+              })
+            }
+          }
+          console.log(res);
+        }
+      })
+    }
+
   }
 })
